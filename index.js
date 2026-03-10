68a66725ce2cb90b423c093aad40ff9eab83c9e2
Somaya Ayman <somaya189ayman@gmail.com>
2026-03-10 04:52:46 +0200

    add intention creation

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+import "dotenv/config";
 import express from "express";
 import  globalError  from "./Middleware/User/CatchError.js";
 import { connection } from "./Database/dbconnect.js";
 import userRoutes from "./Modules/Users/userRoutes.js";
+import paymentRoutes from "./Modules/Payments/payment.routes.js";
 
 const app = express();
 
 
 app.use(express.json());
+app.use("/api/payment", paymentRoutes);
 app.use(userRoutes);
 app.use(globalError);
 
