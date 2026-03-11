fca6712a178e8e9842d365a9d334b81017dd0a0b
shahdmohd <shahdmohd21203@gmail.com>
2026-03-11 22:21:05 +0200

    implementing  product controller + product Routes and Product and Category Models

---
+++ Database/Models/Product.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: [String]//type: array of strings -> bec the product may have more than one image
+    }, 
+     category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category"
+    },
+
+    seller: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    ratingsAverage: { // the avg rating of the product 
+        type: Number,
+        default: 0
+    },
+
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    }
+})
