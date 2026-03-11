0b0c055070902df80784991d60fee431444fb0cb
shahdmohd <shahdmohd21203@gmail.com>
2026-03-11 22:32:31 +0200

    implementing Category controller and routes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import  globalError  from "./Middleware/User/CatchError.js";
 import { connection } from "./Database/dbconnect.js";
 import userRoutes from "./Modules/Users/userRoutes.js";
+import categoryRoutes from "./Modules/Category/Category.Routes.js";
+import productRoutes from "./Modules/Product/Product.Routes.js";
 
 const app = express();
 
@@ -9,7 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(userRoutes);
 app.use(globalError);
-
+app.use(categoryRoutes);
+app.use(productRoutes);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
