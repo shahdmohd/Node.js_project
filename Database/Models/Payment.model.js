e2c39172cd30732c4aa06b72549976047d7b77f0
Somaya Ayman <somaya189ayman@gmail.com>
2026-03-13 23:24:11 +0200

    update  payment model

--- Database/Models/Payment.model.js
+++ Database/Models/Payment.model.js
@@ -1,27 +1,37 @@
 import mongoose from "mongoose";
 
-const orderItemSchema = new mongoose.Schema({
-    price: {
-        type: Number,
-        required: [true, "Price is required"],
-        min: [0.01, "Price must be greater than zero"],
-    },
-    productID: {
+const paymentSchema = new mongoose.Schema({
+    order: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: [true, "Product is required"]
+        ref: "Order",
+        required: [true, "Order ID is required"]
     },
-    quantity: {
-        required: [true, "Quantity is required"],
-        min: [1, "Quantity must be at least 1"],
-        validate: {
-            validator: Number.isInteger,
-            message: "Quantity must be an integer"
-        },
-        required: [true, "Quantity is required"]
+    status: {
+        type: String,
+        enum: ["pending", "completed", "failed", "refunded"],
+        default: "pending"
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "User ID is required"]
+    },
+    currency: {
+        type: String,
+        default: "EGP",
+    },
+    amount: {
+        type: Number,
+        required: [true, "Amount is required"],
+        min: [0.01, "Amount must be greater than zero"]
+    },
+    paidAt: {
+        type: Date
     }
 }, {
-    versionKey: false 
+    timestamps: true,
+    versionKey: false
 });
 
-export const orderItemModel = mongoose.model("OrderItem", orderItemSchema);
+const paymentModel = mongoose.model("Payment", paymentSchema);
+export default paymentModel;
