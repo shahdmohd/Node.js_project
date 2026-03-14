71367e8aeb2205c39cb91dc965814c47bb952ced
Heba Maher <hebamaher25@gmail.com>
2026-03-14 16:25:16 +0200

    added control over categories

--- Modules/Category/Category.Routes.js
+++ Modules/Category/Category.Routes.js
@@ -1,17 +1,18 @@
 import express from "express";
 import { listCategories, addCategory, deleteCategory, getCategoryByID, updateCategory  } from "./Category.Controller.js";
+import { authorizeRoles } from "../../Middleware/roleMiddleware.js";
 
 let categoryRoutes = express.Router();
 
 
-categoryRoutes.post("/category", addCategory);
+categoryRoutes.post("/category", authorizeRoles("seller", "admin"), addCategory);
 
 categoryRoutes.get("/category", listCategories);
 
 categoryRoutes.get("/category/:id", getCategoryByID);
 
-categoryRoutes.put("/category/:id", updateCategory);
+categoryRoutes.put("/category/:id", authorizeRoles("seller", "admin"), updateCategory);
 
-categoryRoutes.delete("/category/:id", deleteCategory);
+categoryRoutes.delete("/category/:id", authorizeRoles("seller", "admin"), deleteCategory);
 
 export default categoryRoutes;
