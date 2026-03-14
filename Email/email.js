07152598f02537a9d63685db80a5434d9a59ec90
Somaya Ayman <somaya189ayman@gmail.com>
2026-03-15 00:09:08 +0200

    add order email notifications

--- Email/email.js
+++ Email/email.js
@@ -1,10 +1,8 @@
 import nodemailer from "nodemailer";
 import jwt from "jsonwebtoken";
 import { template } from "./emailTemplate.js";
+import { orderTemplate } from "./orderTemplate.js";
 
-
-
-export async function sendEmail(email){
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
@@ -13,9 +11,8 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-
+export async function sendEmail(email){
 let emailToken = jwt.sign(email, "emailToken")
-
   const info = await transporter.sendMail({
     from: '"From Note APP" <janna.wael.ali@gmail.com>',
     to: email,
@@ -23,6 +20,19 @@ let emailToken = jwt.sign(email, "emailToken")
    
     html: template(emailToken), 
   });
-
   console.log("Message sent:", info.messageId);
+}
+
+export async function sendOrderEmail(order, action = "placed") {
+  try {
+    await transporter.sendMail({
+      from: '"Your Store" <janna.wael.ali@gmail.com>',
+      to: order.user.email,
+      subject: `Order ${action.charAt(0).toUpperCase() + action.slice(1)} - Order #${order._id}`,
+      html: orderTemplate(order, action)
+    });
+    console.log(`Order ${action} email sent to:`, order.user.email);
+  } catch (error) {
+    console.error("Error sending order email:", error);
+  }
 }
